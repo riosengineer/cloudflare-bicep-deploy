965609087151481e21af5ce4e71b2b382c7b601e
Dan Rios <36534747+riosengineer@users.noreply.github.com>
2025-10-31 22:21:03 +0000

    refactor: remove deprecated CloudFlareCustomRuleHandler file

--- src/Handlers/CloudFlareCustomRuleHandler.cs
+++
@@ -1,4 +0,0 @@
-namespace CloudFlareExtension.Handlers;
-
-// Deprecated: custom WAF rules are no longer supported. This file is retained to avoid breaking
-// existing project references but intentionally contains no implementation.
